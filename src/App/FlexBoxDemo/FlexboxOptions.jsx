06c950865bc5fd3d86ff463cf597c9e71eca78de
funcbind <dotvalueof@gmail.com>
2023-08-16 10:56:32 +0100

    Minor changes for vercel deployments

--- src/App/FlexBoxDemo/FlexboxOptions.jsx
+++ src/App/FlexBoxDemo/FlexboxOptions.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import react from "react";
 import { useDispatch, useSelector } from "react-redux";
-import "./flexboxDemo.scss";
+import "/src/App/FlexBoxDemo/flexboxDemo.scss";
 import {
   additionalDetails,
   defaultProperties,
